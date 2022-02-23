fdab197d7686f742866ec3d5f36c5d0bd4d06b95
libiqi <18612702516@163.com>
2022-02-23 15:24:43 +0800

    修改了不合理的提示文本

--- Cases/DreamerCase/ItemCase.py
+++ Cases/DreamerCase/ItemCase.py
@@ -35,28 +35,28 @@ class ItemCase(BaseCase):
             useType = record.useType
             prompt = record.prompt
             useEff = record.useEff
-            self.flybook_assert(infoType in infoTypeList,'{0}列infoType填写错误，未找到类型 错误值：{1}'.format(i,infoType))
+            self.flybook_assert(infoType in infoTypeList,'Item表infoType填写错误，未找到类型 id：{0} 错误值：{1}'.format(i,infoType))
             if canUse == 1:
-                self.flybook_assert(useType in useTypeList,'{0}列useType填写错误，未找到类型 错误值：{1}'.format(i,useType))
-                self.flybook_assert(useNeed != 0 and useNeed != None,'{0}列的useNeed填写错误,可用道具useNeed不应为0或空 错误值：{1}'.format(i,useNeed))
+                self.flybook_assert(useType in useTypeList,'Item表useType填写错误，未找到类型 id：{0} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useNeed != 0 and useNeed != None,'Item表的useNeed填写错误,可用道具useNeed不应为0或空 id：{0} 错误值：{1}'.format(i,useNeed))
             else:
-                self.flybook_assert(prompt != 1,'{0}列prompt配置错误，不可用道具不应配置红点 错误值：{1}'.format(i,prompt))
+                self.flybook_assert(prompt != 1,'Item表prompt配置错误，不可用道具不应配置红点 id：{0} 错误值：{1}'.format(i,prompt))
             if infoType in [1,6,7]:
-                self.flybook_assert(useType == None,'{0}列useType填写错误，infoType={1}，useType应为空 错误值：{2}'.format(i,infoType,useType))
+                self.flybook_assert(useType == None,'Item表useType填写错误，infoType={1}，useType应为空 id：{0} 错误值：{2}'.format(i,infoType,useType))
             elif infoType == 2:
-                self.flybook_assert(useType != 4 and useType != 5,'{0}列useType填写错误，infoType=2，useType不能为{1} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useType != 4 and useType != 5,'Item表useType填写错误，infoType=2，useType不能为{1} id：{0} 错误值：{1}'.format(i,useType))
             elif infoType == 3:
-                self.flybook_assert(useType == 1,'{0}列useType填写错误，infoType=3，useType不能为{1} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useType == 1,'Item表useType填写错误，infoType=3，useType不能为{1} id：{0} 错误值：{1}'.format(i,useType))
             elif infoType == 4:
-                self.flybook_assert(useType == 2,'{0}列useType填写错误，infoType=4，useType不能为{1} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useType == 2,'Item表useType填写错误，infoType=4，useType不能为{1} id：{0} 错误值：{1}'.format(i,useType))
             elif infoType == 5:
-                self.flybook_assert(useType == 4,'{0}列useType填写错误，infoType=5，useType不能为{1} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useType == 4,'Item表useType填写错误，infoType=5，useType不能为{1}  id：{0}错误值：{1}'.format(i,useType))
             elif infoType == 10:
-                self.flybook_assert(useType == 5,'{0}列useType填写错误，infoType=10，useType不能为{1} 错误值：{1}'.format(i,useType))
+                self.flybook_assert(useType == 5,'Item表useType填写错误，infoType=10，useType不能为{1} id：{0} 错误值：{1}'.format(i,useType))
             if useType == 2:
-                self.flybook_assert(useEff in randomChestIdList,'{0}列useEff填写错误，在Random_Chest表找不到 错误值：{1}'.format(i,useEff))
+                self.flybook_assert(useEff in randomChestIdList,'Item表useEff填写错误，在Random_Chest表找不到 id：{0} 错误值：{1}'.format(i,useEff))
             elif useType == 3:
                 useE3 = useEff.replace('[','').replace(']','')
                 useE3 = useE3.split(',')
                 for j in filter(lambda x:x%2==1,range(len(useE3))):
-                    self.flybook_assert(int(useE3[j]) in gachaRaceRaceidList,'{0}列useEff填写错误，{2}在Gacha_Race表找不到 错误值：{1}'.format(i,useEff,useE3[j]))
+                    self.flybook_assert(int(useE3[j]) in gachaRaceRaceidList,'Item表useEff填写错误，{2}在Gacha_Race表找不到 id：{0} 错误值：{1}'.format(i,useEff,useE3[j]))
